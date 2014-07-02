f83ee03ee2f708ae1876317ce218d795db4aa5dc
Jacob Kodner <jake@kodner.net>
2014-07-02 12:37:54 -0700

    Made functions case-insenstivie

--- spell.py
+++ spell.py
@@ -4,10 +4,13 @@ def find(word, rat=0.8, let=0.75):
 	The 'rat' parameter specifies what Levenshtein word similarity ratio the two words'
 	similarity have to be greater than or equal to (defaults to 0.8). The 'let' parameter 
 	inputs a percentage in decimal form of how many similar letters are shared (defaults to
-	0.75 which is 75%)."""
+	0.75 which is 75%).
+
+	Note: Function is case-insensitive"""
 
 	from Levenshtein import ratio
 	from math import floor
+	word = word.lower()
 	words = []
 	with open('/usr/share/dict/words') as f:
 		for itm in f.readlines():
@@ -29,4 +32,33 @@ def find(word, rat=0.8, let=0.75):
 		result.append(i[1])
 	return result
 
-def find_lst(word, lst, rat=0.8, let=0.75)
+def find_lst(word, lst, rat=0.8, let=0.75):
+	"""This function returns words similar in spelling that are in in the specified 
+	'lst' parameter to the inputted 'word' parameter.
+
+	The 'rat' parameter specifies what Levenshtein word similarity ratio the two words'
+	similarity have to be greater than or equal to (defaults to 0.8). The 'let' parameter 
+	inputs a percentage in decimal form of how many similar letters are shared (defaults to
+	0.75 which is 75%).
+
+	Note: Function is case-insensitive"""
+
+	from Levenshtein import ratio
+	from math import floor
+	word = word.lower()
+	sim = []
+	for i in lst:
+		if ratio(word, i) >= rat:
+				if abs(len(word) - len(i)) <= 2:
+					num = 0
+					for x in i:
+						if x in word:
+							num += 1
+					if num >= floor(len(word) * let):
+						sim.append([ratio(word, i), i])
+	sim.sort()
+	sim = sim[::-1]
+	result = []
+	for i in sim:
+		result.append(i[1])
+	return result
